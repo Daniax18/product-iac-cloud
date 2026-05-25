fc256263f6073fbf18747bca7d9c3edad356b90e
Dania Midera <daniamidera@gmail.com>
2026-05-25 23:41:20 +0400

    Fix availability zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,10 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+  filter {
+    name   = "availabilityZone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
 }
 
 # ─── Key Pair ─────────────────────────────────────────────────────────────────
